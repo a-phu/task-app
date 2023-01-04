d9f6b49e351c67efe22ba24d34b44c6414f8ecfa
a-phu <annabel.phu@hotmail.com>
2023-01-04 16:35:00 +1100

    adding css styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@
 
 
 import {useState} from 'react';
-import { isAccordionItemSelected } from 'react-bootstrap/esm/AccordionContext';
+// import { isAccordionItemSelected } from 'react-bootstrap/esm/AccordionContext';
+import Overview from './components/Overview';
+import {v4 as uuid} from 'uuid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import './App.css';
 
 function App() {
   const [task, setTask] = useState('');
@@ -13,26 +18,35 @@ function App() {
 
   const submitTask = () => {
     console.log("tasks before:", taskList);
-    setTaskList(oldTaskList => [...oldTaskList, task]);
+    setTaskList(oldTaskList => [...oldTaskList, {id: uuid(), text: task}]);
     console.log("tasks after:", taskList);
+    //clear input after submission
+    setTask('');
   }
 
+
+  //makes submission a form
+  //allows for button to be used via click or enter button press
+  const onFormSubmit = (e) => {
+    //this prevents default state to refresh the page 
+    //each time s/t is submitted
+    e.preventDefault();
+  }
+  const heart = <FontAwesomeIcon icon={faHeart} />
+
   return (
-    <div>
-      <h1>task app</h1>
+    <div className="App">
+      <h1>today's to-do</h1>
+      <form onSubmit = {onFormSubmit}>
         <input 
           value = {task}
+          type="text"
           onInput={e => setTask(e.target.value)}
         />
         {console.log("task entered: ", task)}
-        
-        <button onClick = {submitTask}>Submit task</button>
-        <p>
-          {taskList.map((task,index) => {
-            //need to explicitly return
-            return <li key={index}>{task}</li>
-          })}
-        </p>
+        <button type = "submit" onClick = {submitTask}>Submit task</button>
+      </form>
+      <Overview taskList = {taskList} setTaskList = {setTaskList}/>
     </div>
   );
 }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,1 +1,37 @@
 /*render tasks in this file*/
+//need to import react or else will have uncaught ref error "react not defined"
+import React from 'react';
+import './Overview.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+
+const Overview = (props) => {
+    // const deleteIcon = <FontAwesomeIcon icon="fa-solid fa-trash" />;
+    // const {taskList} = props;
+
+    return(
+        
+        <ol>
+            {props.taskList.map((task, index) => {
+                //only return what you need to return
+                //do not return a console log or else program thinks 
+                //task.id is not unique
+                // console.log("task id: ", task.id)
+                return (    
+                    <li key={task.id}>
+                        {task.text+" "} 
+                        <FontAwesomeIcon className="icon-button" icon={faPenToSquare} />
+                        {" "}
+                        <FontAwesomeIcon className="icon-button" icon={faTrash} onClick = {
+                          () => {props.setTaskList(props.taskList.filter((a) => task.id !== a.id));}
+                        }/>
+                    </li>
+
+                );
+            })}
+        </ol>
+    );
+}
+
+export default Overview;
